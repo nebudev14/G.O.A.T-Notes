beff73868fb0a3344802729708d2a05e59620ff0
Fahim <fahimhussain21@gmail.com>
2021-05-29 21:19:05 -0400

    i realized i wasn't staging buttons.js
    browser tab icon

--- background.js
+++ background.js
@@ -1,3 +1,13 @@
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.local.set({'notes': [{caption: "this is a default note!", base64: 'https://stuff.co.za/wp-content/uploads/2020/12/science-of-earworms-explain-why-never-gonna-give-you-up-is-stuck-in-our-heads-30-years-later.png'}]});
+    const timeNow = Date();
+    chrome.storage.local.set({
+        'notes': [
+            {
+                caption: "this is a default note!",
+                base64: 'https://stuff.co.za/wp-content/uploads/2020/12/science-of-earworms-explain-why-never-gonna-give-you-up-is-stuck-in-our-heads-30-years-later.png',
+                url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+                time: timeNow
+            }
+        ]
+    });
 })
--- buttons.js
+++ buttons.js
@@ -4,18 +4,35 @@ const takeNote = () => {
     
     chrome.tabs.captureVisibleTab(null, {}, function(image) {
         chrome.storage.local.get(['notes'], function(result) {
-            console.log(result);
-            // object representing a note
-            note = {
-                caption: document.getElementById("note").value,
-                base64: image
-            }
-            document.getElementById("note").value = "";
-            // updating saved notes
-            let retrievedNotes = result.notes;
-            retrievedNotes.push(note)
-            chrome.storage.local.set({'notes': retrievedNotes}, function() {
-                console.log("new note has been logged!")
+            chrome.tabs.query({ active: true }, tabs => {
+                const url = tabs[0].url;
+                console.log(url)
+                console.log(tabs[0].id)
+                const timeNow = Date();
+
+                // custom functionality for google mets
+                // if (url.startsWith("https://meet.google.com")) {
+                //     const code = 'document.getElementsByClassName("Gv1mTb-aTv5jf Gv1mTb-PVLJEc")'
+                //     chrome.tabs.executeScript(tabs[0].id, { code }, (result) => {
+                //         console.log(result)
+                //         console.log(result[0].getBoundingClientRect());
+                //     });
+                // }
+                console.log(result);
+                // object representing a note
+                note = {
+                    caption: document.getElementById("note").value,
+                    base64: image,
+                    url: url,
+                    time: timeNow
+                }
+                document.getElementById("note").value = "";
+                // updating saved notes
+                let retrievedNotes = result.notes;
+                retrievedNotes.push(note)
+                chrome.storage.local.set({'notes': retrievedNotes}, function() {
+                    console.log("new note has been logged!")
+                });
             });
         });
     });
